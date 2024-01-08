0cd2ced12ca553030908407408b6a9ea2c7a09d9
ruhulxmin <154271498+ruhulxmin@users.noreply.github.com>
2024-01-09 01:09:31 +0600

    Update Remove.py

--- Remove.py
+++ Remove.py
@@ -2,12 +2,14 @@
 #https://github.com/ruhulxmin
 from os import system as clr
 clr('clear')
-print(60*'~')
-print("      =>DUPLICATE ID REMOVER FROM FILE BY REX ")
-print("      =>CODED BY   -  - :RUHUL AMIN BOKSHI ")
-print("      =>Facebook   -  - :Ruhul Xmin BokXi ")
-print("      =>Github     -  - :ruhulxmin ")
-print(60*'~')
+
+print('\33[1;92m╔━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╗')
+print('\33[1;92m║➣\033[1;91m DEVOLPER      : Ruhul Xmin\33[1;92m               ')
+print('\33[1;92m║➣ FACEBOOK      : Ruhul Xmin BokXi    \33[1;92m                  ')
+print('\33[1;92m║➣\033[1;34m GITHUB        : ruhulxmin               \33[1;92m           ')
+print('\33[1;92m║➣\033[1;35m TOOLS         : DUPLICATE ID REMOVER \33[1;92m                  ')
+print('\33[1;92m║➣\033[1;35m VERSION       : V1.0  \33[1;92m                  ')
+print('\33[1;92m╚━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╝')
 input_file = input("INPUT FILE NAME : ")
 output_file = input("OUTPUT FILE NAME : ")
 unique_ids = {}
@@ -28,4 +30,5 @@ with open(input_file, 'r') as infile:
 with open(output_file, 'w') as outfile:
     for line in unique_ids.values():
         outfile.write(line + '\n')
-print(f"DUPLICATE FILE REMOVE SAVE AS : {output_file}")
+print(f"DUPLICATE REMOVED FILE SAVED AS: {output_file}")
+import os; import time; os.system('xdg-open https://facebook.com/ruhul.xmin'); time.sleep(1)
