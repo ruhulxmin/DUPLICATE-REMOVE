1e2556375d96ff8a2254eceb5ed779dc595ccd72
ruhulxmin <154271498+ruhulxmin@users.noreply.github.com>
2024-01-09 00:23:07 +0600

    Remove.py
    Remove Duplicates from your file 🥱

---
+++ Remove.py
@@ -0,0 +1,28 @@
+#coded by ruhul xmin
+#https://github.com/ruhulxmin
+from os import system as clr
+clr('clear')
+print("        DUPLICATE ID REMOVER ")
+print("        CODED BY : MR.DIPTO ")
+print(40*'-')
+input_file = input("INPUT FILE NAME : ")
+output_file = input("OUTPUT FILE NAME : ")
+unique_ids = {}
+with open(input_file, 'r') as infile:
+    for line in infile:
+        line = line.strip()
+        values = line.split('|')
+        if len(values) == 3:
+            id, pas, coki = values
+            key = f"{id}|{pas}|{coki}"
+            if key not in unique_ids:
+                unique_ids[key] = line
+        else:
+            id, pas = values
+            key = f"{id}|{pas}"
+            if key not in unique_ids:
+                unique_ids[key] = line
+with open(output_file, 'w') as outfile:
+    for line in unique_ids.values():
+        outfile.write(line + '\n')
+print(f"DUPLICATE FILE REMOVE SAVE AS : {output_file}")
